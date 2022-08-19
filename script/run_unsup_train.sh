4b4e0e8f89e307f2fb1e7ae81fb5300384714a58
yuji12138 <97024577+yuji12138@users.noreply.github.com>
2022-08-19 18:43:34 +0100

    Update run_unsup_train.sh

--- script/run_unsup_train.sh
+++ script/run_unsup_train.sh
@@ -1,11 +1,11 @@
-CUDA_VISIBLE_DEVICES="0" python -m torch.distributed.launch  --nproc_per_node=1  train.py \
+CUDA_VISIBLE_DEVICES="0,1" python -m torch.distributed.launch  --nproc_per_node=2  train.py \
     --device gpu \
     --output_path output \
     --lr 3e-5 \
     --dropout 0.1 \
     --epochs 2 \
-    --batch_size_train 64 \
-    --batch_size_eval 256 \
+    --batch_size_train 16 \
+    --batch_size_eval 64 \
     --num_workers 0 \
     --eval_step 100 \
     --max_len 64 \
