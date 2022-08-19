a7748a30abbe38d7fc6a9fadd1a48375114880c9
yuji12138 <97024577+yuji12138@users.noreply.github.com>
2022-08-19 18:43:00 +0100

    Add files via upload

---
+++ script/run_unsup_train.sh
@@ -0,0 +1,22 @@
+CUDA_VISIBLE_DEVICES="0" python -m torch.distributed.launch  --nproc_per_node=1  train.py \
+    --device gpu \
+    --output_path output \
+    --lr 3e-5 \
+    --dropout 0.1 \
+    --epochs 2 \
+    --batch_size_train 64 \
+    --batch_size_eval 256 \
+    --num_workers 0 \
+    --eval_step 100 \
+    --max_len 64 \
+    --seed 42 \
+    --train_file data/wiki1m_for_simcse.txt \
+    --dev_file data/stsbenchmark/sts-dev.csv \
+    --test_file data/stsbenchmark/sts-test.csv \
+    --pretrain_model_path princeton-nlp/unsup-simcse-bert-base-uncased \
+    --pooler cls \
+    --train_mode unsupervise \
+    --overwrite_cache \
+    --do_train \
+    --do_predict \
+  
