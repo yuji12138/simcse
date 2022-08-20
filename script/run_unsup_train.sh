58d30750a55edf6ff982fc9040ceea376b6a0c49
yuji12138 <97024577+yuji12138@users.noreply.github.com>
2022-08-20 21:47:58 +0100

    Update run_unsup_train.sh

--- script/run_unsup_train.sh
+++ script/run_unsup_train.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES="0,1,2,3" python -m torch.distributed.launch  --nproc_per_node=4  train.py \
+CUDA_VISIBLE_DEVICES="0,1" python -m torch.distributed.launch  --nproc_per_node=2  train.py \
     --device cuda \
     --output_path output \
     --lr 3e-5 \
